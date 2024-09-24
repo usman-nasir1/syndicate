b9b63e0be8f3aa21e345b646625ba77acfbabd4c
Usman <test@test.com>
2024-09-24 16:38:14 +0500

    commented out the cache

--- tasks/views.py
+++ tasks/views.py
@@ -13,7 +13,7 @@ from django.core.cache import caches
 from tasks.models import Task
 from tasks.forms import TaskForm
 
-@method_decorator(cache_page(60 * 15, cache='memcached'), name='get')
+# @method_decorator(cache_page(60 * 15, cache='memcached'), name='get')
 class TaskList(LoginRequiredMixin, ListView):
     """
     TaskList CBV
@@ -24,8 +24,8 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = "tasks"
 
     def get(self, request, *args, **kwargs):
-        default_cache = caches['memcached']
-        default_cache.set('zaza', 'zaza_value', timeout=300)
+        # default_cache = caches['memcached']
+        # default_cache.set('zaza', 'zaza_value', timeout=300)
         if "list_visit_count" in self.request.session:
             self.request.session["list_visit_count"] += 1
         else:
