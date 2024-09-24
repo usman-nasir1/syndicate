ef45038b8b45be270336a45bd2a6828b1d9e878b
Usman <test@test.com>
2024-09-25 01:47:12 +0500

    added signals

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,6 +1,27 @@
 from django.apps import AppConfig
-
+from django.core.signals import request_finished, request_started
+from django.core.handlers.wsgi import WSGIHandler
+from django.dispatch import receiver
 
 class TasksConfig(AppConfig):
+    """
+    Configuration for the tasks app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        request_started.connect(request_started_handler, sender=WSGIHandler)
+
+def request_started_handler(sender, **kwargs):
+    """
+    Request Started Handler
+    """
+    print(".............Request Started.............")
+
+@receiver(request_finished, sender=WSGIHandler)
+def request_finished_handler(sender, **kwargs):
+    """
+    Request Finished Handler
+    """
+    print(".............Request Finished.............")
--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,11 @@
+import logging
 from django.db import models
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from base_model import BaseModel
+
+logger = logging.getLogger("finances")
 
 class Task(BaseModel):
     """
@@ -18,3 +23,21 @@ class Task(BaseModel):
     assigned_to = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES)
     attachment = models.FileField(upload_to="tasks", blank=True, null=True)
+
+@receiver(post_delete, sender=Task)
+def task_post_delete(sender, instance, **kwargs):
+    """
+    Request Started Handler
+    """
+    print("TASK DELETED..............")
+    # logger.debug("Task deleted %s %s ", sender, kwargs)
+
+# @receiver(pre_save, sender=Task)
+# def task_pre_save(sender, **kwargs):
+#     """
+#     Request Started Handler
+#     """
+#     print("TASK Saved..............")
+#     print(sender)
+#     print(kwargs)
+    # logger.debug("Task deleted %s %s ", sender, kwargs)
